9e0e45e7e1242e7861f75a5b764555f6b1f66b3b
Mariantonnia <66175651+Mariantonnia@users.noreply.github.com>
2025-03-09 20:50:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain import LLMChain
 from langchain_groq import ChatGroq
-import os, getpass
+import os
 os.environ["GROQ_API_KEY"] = "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"
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "langchain-academy"
