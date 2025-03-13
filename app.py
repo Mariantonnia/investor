96eb0586799adf79cf5e69210db7f76e28df5141
Mariantonnia <66175651+Mariantonnia@users.noreply.github.com>
2025-03-13 19:05:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import os
 import re
 import matplotlib.pyplot as plt
 import uuid
+from dotenv import load_dotenv
+load_dotenv()
 
 # Configurar el modelo LLM
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
